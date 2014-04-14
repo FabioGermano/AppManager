c0b6dc29954111842bcdc74f648f0dd8b0bdbb6b
khalidb91 <khalid.benni@gmail.com>
2014-04-14 14:58:51 +0200

    ora dovrebbe andare

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 		echo getcwd();
 	echo "</h1></div>";
 	echo "<div>";
-	$dir    = '../';
+	$dir    = $_GET['dir'];
 	$files1 = scandir($dir);
 
 	foreach($files1 as $element)
 	{
-		 echo "<a href='".$elemnt."'> ".$elemnt."</a><br>";
+		 echo "<a href='index.php?dir=".$element."'> ".$element."</a><br>";
 	}
 	echo "</div>";
 ?>
