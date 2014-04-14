ee54af4291148c27e6f66d41dfaa90ccd6bf1118
khalidb91 <khalid.benni@gmail.com>
2014-04-14 14:34:49 +0200

    aggiunto index.php che visualizza links

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+	echo "<div><h1> Testing OpsWorks</h1></div>";
+	echo "<div>";
+	$dir    = '../';
+	$files1 = scandir($dir);
+
+	foreach($files as $element)
+	{
+		 echo "<a href='".$elemnt."'> ".$elem."</a><br>";
+	}
+	echo "</div>";
+?>
